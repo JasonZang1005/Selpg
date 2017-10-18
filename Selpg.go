249f9e0c1f5ea0ebb5d1349cb3dab6dfd566eca4
JasonZang1005 <18142823152@163.com>
2017-10-18 11:10:14 +0800

    Update Selpg.go

--- Selpg.go
+++ Selpg.go
@@ -21,73 +21,64 @@ type Args struct {
 
 func main() {
     var arg Args
+    arg.programName=os.Args[0]
+    flag.IntVar(&arg.startPos,"s",-1,"start position")
+    flag.IntVar(&arg.endPos,"e",-1,"end position")
+    flag.IntVar(&arg.pageLen,"l",-1,"number of lines")
+    flag.BoolVar(&arg.pageType,"f",false,"/f")
+    flag.StringVar(&arg.destination,"d","","specify destination")
 
-    ParseArgs(&arg)
-    processArgs(&arg)
-    FileProcessing(&arg)
+    flag.Parse()
 
-}
-
-func ParseArgs(arg *Args) {
-  arg.programName=os.Args[0]
-  flag.IntVar(&arg.startPos,"s",-1,"start position")
-  flag.IntVar(&arg.endPos,"e",-1,"end position")
-  flag.IntVar(&arg.pageLen,"l",-1,"number of lines")
-  flag.BoolVar(&arg.pageType,"f",false,"/f")
-  flag.StringVar(&arg.destination,"d","","specify destination")
-
-  flag.Parse()
-}
-
-func processArgs(arg *Args){
-
-  if  arg.startPos<0 || arg.endPos<0 {
-    showErr("invalid start number or end number")
-  }
-
-  if arg.startPos>arg.endPos {
-    showErr("start number is should be smaller than end number")
-  }
-
-  arg.src = flag.Arg(0)
-
-  if arg.pageType==true{
-    if arg.pageLen!=-1{
-        showErr("only one type is allowed")
+    if  arg.startPos<0 || arg.endPos<0 {
+      showErr("invalid start number or end number")
     }
-  }else{
-    if arg.pageLen<1{
-      arg.pageLen=72
+
+    if arg.startPos>arg.endPos {
+      showErr("start number is should be smaller than end number")
     }
-  }
+
+    arg.src = flag.Arg(0)
+
+    if arg.pageType==true{
+      if arg.pageLen!=-1{
+          showErr("only one type is allowed")
+      }
+    }else{
+      if arg.pageLen<1{
+        arg.pageLen=72
+      }
+    }
+    if flag.NArg()==1{
+      arg.src=flag.Arg(0)
+    }
+
+    if arg.src==""{
+      reader:=bufio.NewReader(os.Stdin)
+      if arg.pageType==true{
+        page(reader,&arg)
+      }else{
+        line(reader,&arg)
+      }
+    }else{
+      file,err:=os.Open(arg.src)
+      reader:=bufio.NewReader(file)
+      CheckErr(err)
+      if arg.pageType==true{
+        page(reader,&arg)
+      }else{
+        line(reader,&arg)
+      }
+    }
 }
+
+
+
 func showErr( info string) {
-  fmt.Println(info)
+  fmt.Fprintf(os.Stderr,info)
   os.Exit(1)
 }
-func FileProcessing(arg *Args){
-  if flag.NArg()==1{
-    arg.src=flag.Arg(0)
-  }
 
-  if arg.src==""{
-    reader:=bufio.NewReader(os.Stdin)
-    if arg.pageType==true{
-      page(reader,arg)
-    }else{
-      line(reader,arg)
-    }
-  }else{
-    file,err:=os.Open(arg.src)
-    reader:=bufio.NewReader(file)
-    CheckErr(err)
-    if arg.pageType==true{
-      page(reader,arg)
-    }else{
-      line(reader,arg)
-    }
-  }
-}
 
 func line(reader *bufio.Reader, arg *Args){
   NumOfLine :=1
@@ -99,10 +90,11 @@ func line(reader *bufio.Reader, arg *Args){
         fmt.Println(line)
       }else{
         cmd := exec.Command("./out")
-				pipe, err := cmd.StdinPipe()
+				echoInPipe, err := cmd.StdinPipe()
         CheckErr(err)
-				pipe.Write([]byte(line))
-				pipe.Close()
+				echoInPipe.Write([]byte(line))
+        fmt.Println(line)
+				echoInPipe.Close()
 				cmd.Stdout = os.Stdout
 				cmd.Run()
       }
@@ -112,6 +104,12 @@ func line(reader *bufio.Reader, arg *Args){
     }
     NumOfLine++
   }
+  if arg.startPos > NumOfLine/arg.pageLen+1 {
+		fmt.Printf("Start page is greater than end page/n")
+	}
+	if arg.endPos > NumOfLine/arg.pageLen+1 {
+		fmt.Printf("end page is greater than page num/n")
+	}
 }
 
 func page(reader *bufio.Reader, arg *Args){
@@ -123,12 +121,12 @@ func page(reader *bufio.Reader, arg *Args){
       if arg.destination==""{
         fmt.Println(page)
       }else{
-          cmd := exec.Command("./out")          
-  				pipe, err := cmd.StdinPipe()   
-  				CheckErr(err)                          
-  				pipe.Write([]byte(page + "\n")) 
-  				pipe.Close()
-  				cmd.Stdout = os.Stdout               
+          cmd := exec.Command("./out")          // 创建命令"./out"
+  				echoInPipe, err := cmd.StdinPipe()    // 打开./out的标准输入管道
+  				CheckErr(err)                            // 错误检测
+  				echoInPipe.Write([]byte(page + "\n")) // 向管道中写入文本
+  				echoInPipe.Close()                    // 关闭管道
+  				cmd.Stdout = os.Stdout                // ./out将会输出到屏幕
   				cmd.Run()
       }
 
@@ -139,6 +137,13 @@ func page(reader *bufio.Reader, arg *Args){
 
     }
   }
+  if arg.startPos > NumOfPage {
+		fmt.Printf("Start page is greater than end page/n")
+	}
+
+	if arg.endPos > NumOfPage {
+		fmt.Printf("end page is greater than page num/n")
+	}
 }
 
 func CheckErr(err error) {
